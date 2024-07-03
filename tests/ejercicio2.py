d0d7aa474bda66091c9eb09e345d817be0231f9d
Antony16 <136536376+Sh3ccid@users.noreply.github.com>
2024-07-03 14:14:08 -0500

    Update ejercicio2.py

--- tests/ejercicio2.py
+++ tests/ejercicio2.py
@@ -27,23 +27,22 @@ class ParallelState:
 
     def execute(self, data):
         print(f"Ejecutando estado paralelo: {self.name}")
-        threads = []     # alamacenar los hilos inciados
+        threads = []    # alamacenar los hilos inciados
         results = {}
 
-        #Iniciar cada tarea en un hilo separado
+        # Iniciar cada tarea en un hilo separado
         for task_name, task_func in self.tasks.items():
             thread = threading.Thread(target=self.run_task, args=(task_name, task_func, data, results))
             threads.append(thread)
             thread.start()
 
-        #Esperar a que todos los hilos terminen
+        # Esperar a que todos los hilos terminen
         for thread in threads:
             thread.join()
 
         return self.next_state, data
 
     def run_task(self, task_name, task_func, data, results):
-
         results[task_name] = task_func(data)
 
         # Agregar la tarea completada al conjunto de tareas completadas
@@ -52,21 +51,20 @@ class ParallelState:
     def get_completed_tasks(self):
         return list(self.completed_tasks)
 
-
 class ChoiceState:
     '''
     Modela el tipo de estado dentro de una máquina de estados,
     donde toma deciciones basadas en condiciones específicas
     '''
     def __init__(self, name, choices):
-        self.name = name
-        self.choices = choices
-
-    def execute(self, data):
         '''
         Evalúa las condiciones definidas y devuelve el próximo estado basado en los datos
         de entrada
         '''
+        self.name = name
+        self.choices = choices
+
+    def execute(self, data):
         print(f"Evaluando decisiones en estado: {self.name}")
 
         # Itera todas las condiciones
@@ -79,39 +77,39 @@ class ChoiceState:
         return "End", data  #Retorna el siguiente estado y los datos actualizados
 
 # Funciones de tarea específicas para procesamiento paralelo
-def proceso_1(data):
-    print("\nProcesando datos desde la base de datos... \n")
-    time.sleep(1)  # Simulación de proceso de carga
-    data["datos_procesados_1"] = True
+def Proceso_1(data):
+    print("\nCargando datos desde la base de datos... \n")
+    time.sleep(1)  # Simulación de proceso_1
+    data["Proceso1"] = True
     return data
 
-def proceso_2(data):
-    print("Procesando datos desde la base de datos... \n")
-    time.sleep(3)  # Simulación de proceso de limpieza
-    data["datos_procesados_2"] = True
+def Proceso_2(data):
+    print("Limpiando y preprocesando datos... \n")
+    time.sleep(3)  # Simulación de proceso_2
+    data["Proceso2"] = True
     return data
 
-def proceso_3(data):
-    print("Procesando datos desde la base de datos... \n")
-    time.sleep(5)  # Simulación de proceso de análisis
-    data["datos_procesados_3"] = True
+def Proceso_3(data):
+    print("Realizando análisis estadístico de los datos... \n")
+    time.sleep(5)  # Simulación de proceso_2
+    data["Proceso3"] = {"media": 25.4, "desviacion_std": 4.2}
     return data
 
-#Funciones de condición para ChoiceState
+# Funciones de condición para ChoiceState
 def verificar_datos_cargados(data):
-    return data.get('datos_procesados', False)
+    return data.get('Proceso1', False)
 
 def verificar_datos_limpios(data):
-    return data.get('datos_procesados', False)
+    return data.get('Proceso2', False)
 
 # Función independiente para el final del procesamiento
 def end_task(data):
-    if all(key in data for key in ['datos_procesados_1', 'datos_procesados_2', 'datos_procesados_3']):
+    if all(key in data for key in ['Proceso1', 'Proceso2', 'Proceso3']):
         print(f"Final result: {data.get('resultados_analisis', 'Data processed')}")
     else:
         print("No se han completado todas las tareas necesarias.")
 
-#Definición de los estados en la máquina de estados
+# Definir estados y sus tareas paralelas
 states = {
     "espera": WaitState("espera", 2, "decision"),
     "decision": ChoiceState("decision", [
@@ -119,9 +117,9 @@ states = {
         (verificar_datos_limpios, "End")
     ]),
     "procesamiento_datos": ParallelState("procesamiento_datos", {
-        "proceso_1": proceso_1,
-        "proceso_2": proceso_2,
-        "proceso_3": proceso_3
+        "Proceso_1": Proceso_1,
+        "Proceso_2": Proceso_2,
+        "Proceso_3": Proceso_3
     }, "End"),
     "End": TaskState("End", lambda data: (None, data))
 }
@@ -132,13 +130,12 @@ positions = {
     "espera": (0, -2),
     "decision": (0, -4),
     "procesamiento_datos": (0.0055, -5),
-    "proceso_1": (0.0035, -7.5),
-    "proceso_2": (0.0055, -7.5),
-    "proceso_3": (0.0075, -7.5),
+    "Proceso_1": (0.0035, -7.5),
+    "Proceso_2": (0.0055, -7.5),
+    "Proceso_3": (0.0075, -7.5),
     "End": (0, -11)
 }
 # Crear la máquina de estados con los estados definidos y la posición inicial
-
 sm = StateMachine(states, "espera", positions)
 
 # Añadir transiciones entre estados
@@ -146,15 +143,15 @@ sm.add_transition("Start", "espera")
 sm.add_transition("espera", "decision")
 sm.add_transition("decision", "procesamiento_datos", "$cargados")
 sm.add_transition("decision", "End", "$limpios")
-sm.add_transition("procesamiento_datos", "cargar_datos")
-sm.add_transition("procesamiento_datos", "limpiar_datos")
-sm.add_transition("procesamiento_datos", "analizar_datos")
-sm.add_transition("proceso_1", "End")
-sm.add_transition("proceso_2", "End")
-sm.add_transition("proceso_3", "End")
+sm.add_transition("procesamiento_datos", "Proceso_1")
+sm.add_transition("procesamiento_datos", "Proceso_2")
+sm.add_transition("procesamiento_datos", "Proceso_3")
+sm.add_transition("Proceso_1", "End")
+sm.add_transition("Proceso_2", "End")
+sm.add_transition("Proceso_3", "End")
 
 # Datos iniciales para la ejecución
-data = {"datos_cargados": True, "datos_limpios": True}
+data = {"Proceso1": True, "Proceso2": True}
 print("Executing Start")
 final_state, final_data = sm.execute(data)  #Ejecución de la máquina de estados con los datos iniciales
 print(final_data)
